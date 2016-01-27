f1c41d852ffa6b8d288a6320d52d547697597f1d
Kailash Nadh <kailash.nadh@gmail.com>
2016-01-27 15:55:02 +0530

    better setup.py, pypi ready

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
+#!/usr/bin/env python
+
 from setuptools import setup
 
-# requirements
-install_requires = [
-	"requests"
-]
-
-# meta
 setup(
-	name="Kite Connect  Client",
-	version="2.3",
-	author="Kailash Nadh",
-	description="Client for the Kite Connect",
-	install_requires=install_requires,
-	packages=["kiteclient"]
+	name="kiteconnect",
+	version="2.4",
+	description="The official Python client for the Kite Connect trading API",
+	author="Rainmatter Technology (India)",
+	author_email="talk@rainmatter.com",
+	url="https://kite.trade",
+	packages=["kiteconnect"],
+	download_url="https://github.com/rainmattertech/pykiteconnect",
+	license="MIT",
+	classifiers=[
+		"Development Status :: 4 - Beta",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Financial and Insurance Industry",
+		"Programming Language :: Python",
+		"Natural Language :: English",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Topic :: Office/Business :: Financial :: Investment",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Software Development :: Libraries"
+	],
+	install_requires=["requests"]
 )
