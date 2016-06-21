d48dceb49d49eb76104d97ca1c6b0edc35f49c99
Kailash Nadh <kailash.nadh@gmail.com>
2016-06-21 18:29:35 +0530

    bumped version

--- kiteconnect/__init__.py
+++ kiteconnect/__init__.py
@@ -530,7 +530,7 @@ class KiteConnect(object):
 		elif "csv" in r.headers["content-type"]:
 			return r.content
 		else:
-			raise ex.DataException("Unknown Content-Type (%s) in response" % (r.headers["content-type"],))
+			raise ex.DataException("Unknown Content-Type (%s) in response: (%s)" % (r.headers["content-type"], r.content))
 
 
 class WebSocket(object):
