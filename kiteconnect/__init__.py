b44ca84674a8b04ebed5f6d6a38b16e660d27df9
Vivek r <vivek.r@zerodha.com>
2017-07-17 16:41:06 +0530

    Added websocket reconnect example to documentation

--- kiteconnect/__init__.py
+++ kiteconnect/__init__.py
@@ -592,6 +592,14 @@ class WebSocket(object):
 		kws.on_tick = on_tick
 		kws.on_connect = on_connect
 
+		# To enable auto reconnect WebSocket connection in case of network failure
+		# - First param is interval between reconnection attempts in seconds.
+		# Callback `on_reconnect` is triggered on every reconnection attempt. (Default interval is 5 seconds)
+		# - Second param is maximum number of retries before the program exits triggering `on_noreconnect` calback. (Defaults to 50 attempts)
+		# Note that you can also enable auto reconnection	 while initialising websocket.
+		# Example `kws = WebSocket("your_api_key", "your_public_token", "logged_in_user_id", reconnect=True, reconnect_interval=5, reconnect_tries=50)`
+		kws.enable_reconnect(reconnect_interval=5, reconnect_tries=50)
+
 		# Infinite loop on the main thread. Nothing after this will run.
 		# You have to use the pre-defined callbacks to manage subscriptions.
 		kws.connect()
