5dbdac674692b67f8f08627453b145c4d24ac32f
Vivek R <vividvilla@gmail.com>
2018-05-29 16:20:49 +0530

    Rename cmd flag root to root-url for integrated tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -13,14 +13,14 @@ def pytest_addoption(parser):
     """Add available args."""
     parser.addoption("--api-key", action="store", default="Api key")
     parser.addoption("--access-token", action="store", default="Access token")
-    parser.addoption("--root", action="store", default="")
+    parser.addoption("--root-url", action="store", default="")
 
 
 def pytest_generate_tests(metafunc):
     """This is called for every test. Only get/set command line arguments. If the argument is specified in the list of test "fixturenames"."""
     access_token = metafunc.config.option.access_token
     api_key = metafunc.config.option.api_key
-    root = metafunc.config.option.root
+    root_url = metafunc.config.option.root_url
 
     if "access_token" in metafunc.fixturenames and access_token is not None:
         metafunc.parametrize("access_token", [access_token])
@@ -28,11 +28,11 @@ def pytest_generate_tests(metafunc):
     if "api_key" in metafunc.fixturenames and api_key is not None:
         metafunc.parametrize("api_key", [api_key])
 
-    if "root" in metafunc.fixturenames and root is not None:
-        metafunc.parametrize("root", [root])
+    if "root_url" in metafunc.fixturenames and root_url is not None:
+        metafunc.parametrize("root_url", [root_url])
 
 
 @pytest.fixture()
-def kiteconnect(api_key, access_token, root):
+def kiteconnect(api_key, access_token, root_url):
     """Init Kite connect object."""
-    return KiteConnect(api_key=api_key, access_token=access_token, root=root or None)
+    return KiteConnect(api_key=api_key, access_token=access_token, root=root_url or None)
