04574aa38245b24f6f4f119e2cb8e32da172837b
Vivek R <vividvilla@gmail.com>
2021-01-28 11:43:35 +0530

    fix: freeze autobahn dep since api is broken on Python v3

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
         "pyOpenSSL>=17.5.0",
         "enum34>=1.1.6",
         "python-dateutil>=2.6.1",
-        "autobahn[twisted]>=17.10.1"
+        "autobahn[twisted]==19.11.2"
     ],
     tests_require=["pytest", "responses", "pytest-cov", "mock", "flake8"],
     test_suite="tests",
